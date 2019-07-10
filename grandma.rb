a45160aecc58c989849f936e27c70d0d1ffa448b
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 19:47:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
-    puts 'I LOVE YOU TOO PUMPKIN!'
+     'I LOVE YOU TOO PUMPKIN!'
   elsif talk == talk.upcase
-    puts "NO, NOT SINCE 1938!"
+     "NO, NOT SINCE 1938!"
   else
-    puts  "HUH?! SPEAK UP, SONNY!"
+      "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
